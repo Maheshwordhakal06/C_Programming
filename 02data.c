9d35dc6da579697c5ab667ef2054d26703ed31f9
Maheshwordhakal06 <Maheshwordhakal24@gmail.com>
2024-12-01 18:49:03 +0545

    Add files via upload

---
+++ 02data.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+	int firstnumber, secondnumber, add, sum, division;
+	add=firstnumber+secondnumber;
+	sum=firstnumber-secondnumber;
+	division=firstnumber/secondnumber;
+	printf("enter tne first number: \n");
+	scanf("%d,&first,&firstnumber");
+		printf("enter tne second number: \n");
+	scanf("%d,&first,&secondnumber");
+	
+	
+	return 0 ;
+}
