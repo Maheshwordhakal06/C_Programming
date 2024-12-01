bd78de7d106810bdc857ccafb67d3635f9babbc1
Maheshwordhakal06 <Maheshwordhakal24@gmail.com>
2024-12-01 19:39:20 +0545

    Add files via upload

---
+++ consonant and vowel.c
@@ -0,0 +1,47 @@
+// To find the enter number is vowel or consonant
+#include<stdio.h>
+int main(){
+	char x;
+	printf("enter any english alphapets\n ");
+	scanf("%c",&x);
+	switch(x)
+	{
+		
+	case 'A':
+	case'a':
+	case'E':
+	case'e':
+	case'I':
+	case'i':
+	case'O':
+	case'o':
+		case 'U':
+			case 'u':
+				printf("%c is an vowel letter",x );
+				break;
+				default:
+					printf("%c is consonant",x);
+					
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+	
+	
+	
+}
